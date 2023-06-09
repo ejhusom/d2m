64be4a9733f24411a039a75581c5984ef4b30068
EJ <ejlgkvam@online.no>
2023-06-09 14:09:01 +0200

    add

--- src/llm.py
+++ src/llm.py
@@ -45,10 +45,13 @@ The learning method is {learning_method}.
 The 5 most important features are {shap_importances["feature"][0:5].tolist()}.
 """
 
-    user_message += """\
-Can you explain the model to me? Additionally, can you
-explain why the model makes the predictions it makes? Can you also explain
-why the model thinks the above features are important?"""
+    # user_message += f"""\
+# Can you explain the model to me? Additionally, can you
+# explain why the model makes the predictions it makes? Can you also explain
+# why the most important features have influence on the target variable {target}?"""
+    user_message += f"""\
+Why are the most important features important for the target variable {target}?
+Can you reason about why exactly those features have influence on the target variable {target}?"""
 
     system_message = f"""\
 You are an AI assistant that is trying to help a user understand the output
@@ -58,10 +61,12 @@ prediction based on the input data. The prediction is a number or a
 classification. The ML pipeline also produces a feature importance ranking
 for the input data. The feature importance ranking is a list of the input
 features, sorted by how important they are for the predictions. Your role is to
-produce a natural language explanation and reasoning about the ML model. The
-explanation and reasoning should be understandable by a non-technical user. The
-target value is what the ML model is trying to predict.
+produce a natural language explanation and reasoning about the ML model and the
+most important features.
 """
+# The
+# explanation and reasoning should be understandable by a non-technical user. The
+# target value is what the ML model is trying to predict.
 
     print(user_message)
     print(system_message)
